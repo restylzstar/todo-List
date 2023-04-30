dea4ac549226f3541a35e6d482d7e9f00c3db4cd
restylzstar <restylzstar@gmail.com>
2023-04-30 13:35:13 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { TodoWrapper } from './Components/TodoWrapper';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h6>Pawaan, Restyl Jhon C.</h6>
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
